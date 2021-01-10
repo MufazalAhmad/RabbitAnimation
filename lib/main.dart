a3aaee1503d9b5bcde4c16098fa235c5671edf71
mufazal2005 <mufazalahmad2005@gmail.com>
2021-01-10 14:43:19 +0500

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:rabbit_pic/rabbit.dart';
 
-import 'rabbit2.dart';
 
 void main() {
   runApp(MyApp());
